79c27253e9a682ecb6c8f0b13415bdd19aa12c9a
Mario Seijo <1@marioseijo.com>
2020-11-23 13:58:41 -0800

    fixed the clock to display the seconds

--- js/kiosk.js
+++ js/kiosk.js
@@ -1,24 +1,21 @@
-// set things up
 
+// Load time.html into the iframe
 function  loadTime() {
-
   document.getElementById('dynamicContent').src =  "time.html";
 }
 
+// Load calendar.html into the iframe
 function  loadCalendar() {
-
-  document.getElementById('dynamicContent').src =  "calendar.html";
+  document.getElementById('dynamicContent').src =  "calendar.html";  
 }
 
-
+// Load weather.html into the iframe
 function  loadWeather() {
-
   document.getElementById('dynamicContent').src =  "weather.html";
 }
 
-
+// Load traffic.html into the iframe
 function  loadTraffic() {
-
   document.getElementById('dynamicContent').src =  "traffic.html";
 }
 
--- js/time.js
+++ js/time.js
@@ -1,19 +1,23 @@
 // display time
+
+var timeoutID
+
 function displayTime() {
 
   const dt = new Date();
   const options = {
           timeZone: "America/Los_Angeles",
           hour12: true,
-          weekday: 'short',
+          weekday: 'long',
           // year: 'numeric',
-          month: 'short',
+          month: 'long',
           day: 'numeric',
           hour: 'numeric',
-          minute: 'numeric'
+          minute: 'numeric',
+          second: 'numeric',
         };
         document.getElementById('time').innerText = dt.toLocaleString('en-US', options);
       }
 
-// call the function 
-displayTime();
+// call the function every 1 second
+setInterval(displayTime, 1000);
