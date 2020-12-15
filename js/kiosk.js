164c539ce5bc2df2ddcc3d3e8d806ef7f1ff729b
Mario Seijo <1@marioseijo.com>
2020-12-14 16:07:39 -0800

    touched, getting ready to test in prod

--- js/kiosk.js
+++ js/kiosk.js
@@ -1,20 +1,26 @@
 
+///////////////////////////////////////////////
+//
+//    navigation functions
+//
+///////////////////////////////////////////////
+
 // Load time.html into the iframe
 function  loadTime() {
-  document.getElementById('dynamicContent').src =  "time.html";
+  document.getElementById('dynamicContent').src = "time.html";
 }
 
 // Load calendar.html into the iframe
 function  loadCalendar() {
-  document.getElementById('dynamicContent').src =  "calendar.html";
+  document.getElementById('dynamicContent').src = "calendar.html";  
 }
 
 // Load weather.html into the iframe
 function  loadWeather() {
-  document.getElementById('dynamicContent').src =  "weather.html";
+  document.getElementById('dynamicContent').src = "weather.html";
 }
 
 // Load traffic.html into the iframe
 function  loadTraffic() {
-  document.getElementById('dynamicContent').src =  "traffic.html";
+  document.getElementById('dynamicContent').src = "traffic.html";
 }
